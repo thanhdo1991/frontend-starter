706c34c174ae116ac4c5c21c339f5caf01fd930f
Thanh Do <thanh.do@ffwagency.com>
2018-03-22 09:17:01 +0700

    Add sj template

---
+++ sj-template/assets/js/scripts.js
@@ -0,0 +1,34 @@
+// JavaScript
+
+// If there's a `data-background-image` attribute, replace the block
+// background image with the value, inside `url([path])`
+(function ( $ ) {
+
+  var setCustomBgImg = function(el) {
+    el = $(el);
+    var imgUrl = el.attr('data-background-image');
+    imgUrl != "" && (imgUrl = "url(" + imgUrl + ")");
+    el.css('background-image', imgUrl);
+  };
+
+  $("[data-background-image]").each(function(k, el) {
+    setCustomBgImg(el);
+
+    $(el).change(function() {
+      setCustomBgImg(el);
+    });
+  });
+
+  // Background color.
+  var setCustomBgColor = function(el) {
+    el = $(el);
+    var color = el.data('background-color');
+    color != "" && (color = "#" + color);
+    el.css('background-color', color);
+  }
+
+  $("[data-background-color]").each(function(k, el) {
+    setCustomBgColor(el);
+  });
+
+}( jQuery ));
